6e3ea3ee250bc6040f5dddf58bf4d985c1c5300e
LinusdP <linus.de.petris@gmail.com>
2021-11-18 18:35:17 +0100

    Added comments

--- p5/exempel-13-noise_vs_random/sketch.js
+++ p5/exempel-13-noise_vs_random/sketch.js
@@ -10,39 +10,63 @@ var soundPlaying = false;
 
 function setup() {
   createCanvas(950, 500);
+
+  //Sätt pixeldensiteten till 1 för att göra det enklare att läsa av pixlarna i bilden
   pixelDensity(1);
+
+  //Skapa en oscilator för ljudet
   osc = new p5.Oscillator('sine');
 
   background(0);
   noStroke();
+
+  //Skapa en fyrkan med slumpmässiga pixlar (Random)
   randomSquare();
+
+  //Skapa en fyrkan med slumpmässiga pixlar (Noise)
   noiseSquare();
 
+  //Läs in alla pixlar (P5 funktion som skapar en array pixels med alla pixlar)
   loadPixels();
 }
 
 function draw() {
+  // Den pixelarray som skapas via loadPixels() håller alla pixlar i canvasenp5.BandPass()
+  // Arrayen blir 4 ggr så lång som antalet pixlar eftersom R, G, B och Alpha-värdena lagras var för sig efter varandra för varje pixel
+  // För att välja rätt position i arrayen baserat på muspekaren måste vi alltså multiplicera bredden med muspekarens y-potition
+  // och sedan lägga till x-positionen. Detta multipliceras sedan med 4 på grund av ovanstående, (varje pixel representeras med fyra värden)
   let off = (mouseY * width + mouseX) * 4;
+
+  // Läs ut första värder (Röd) för pixeln i fråga (eftersom det är gråskala kommer grän och blå ha samma värde)
   let pix = pixels[off];
+
+  // Välj en frekvens för ljudet baserat på pixelns färgintensitet
   let freq = map(pix, 0, 255, 100, 500);
+
+  //Sätt ljudstyrkan till 1
   let amp = 1;
 
+  // Om ljudet är igång (dvs någon håller ner musknappen)
+  // Då ska vi spela upp ljudet på vald frekvens
   if (soundPlaying) {
     osc.freq(freq, 0.1);
     osc.amp(amp, 0.1);
   }
 }
 
+// Funkmtion som anropas när någon trycker ner musknappen
 function mouseReleased() {
   osc.amp(0, 0.5);
   soundPlaying = false;
 }
 
+// Funktio som anropas när någon släpper musknappen
 function mousePressed() {
   osc.start();
   soundPlaying = true;
 }
 
+// Funktion som ritar ut en kvadrat med slumpmässiga pixlar (Random)
 function randomSquare() {
     for( let y=50 ; y<=450 ; y++ ){
       for( let x=50 ; x<=450 ; x++ ){
@@ -52,6 +76,7 @@ function randomSquare() {
     }
 }
 
+// Funktion som ritar ut en kvadrat med slumpmässiga pixlar (Noise)
 function noiseSquare() {
     for( let y=50 ; y<=450 ; y++ ){
       for( let x=500 ; x<=900 ; x++ ){
