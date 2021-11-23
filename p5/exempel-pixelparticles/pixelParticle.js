d13dcea19e3816ee3eca6b0ce8013e00b2cc5cec
JonasSvegland <jonas@svegland.se>
2021-11-23 11:39:03 +0100

    Commented the code for the pixel particle example
    Commented the code for the pixel particle example and made some simplifications

--- p5/exempel-pixelparticles/pixelParticle.js
+++ p5/exempel-pixelparticles/pixelParticle.js
@@ -1,56 +1,86 @@
+/*
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved.  This file is offered as-is,
+without any warranty.*/
+
+// PixelParticle class
 class PixelParticle {
 
+    // private class members that controls the significance of the movement from the target
+    #fromTargetScale = 0.08;
+    // private class members that controls the significance of the movement to the original position
+    #toOriginalScale = 0.01;
+
+    // constructor used whe instanciating objects of this class
     constructor(x, y, c) 
     {
-      // Skapa stjärnan på en slumpmässig position
-      // Eftersom vi kommer att börja rita från centrum av ritytan ska
-      // positionen vara mellan - halva bredden/höjden och  + halva höjden/bredden
-      this.age = 0;
-
+      // create a vector object to hold the current x and y coordinates
       this.position = createVector(x, y);
+      // create a vector object to hold the original x and y coordinates
       this.originalPosition = createVector(x, y);
-      this.c = c;
-      //print("PixelParticle created with pos " + this.position + " and color " +  this.color);    
+      this.particleColor = c;
+      // Debug the created PixelParticle object, uncomment the line below if needed
+      // print("PixelParticle created with pos " + this.position + " and color " +  this.color);    
     }
 
+    // Scramble method (functions is called methods when part of a class)
+    // This function randomizes the position of the pixel particle basedc on 
+    // the w and h parameters
     scramble(w, h) {
+
       this.position.x = random(-w/2, w/2);
       this.position.y = random(-h/2, h/2);
-      
     }
   
-    move(target) 
-    {
-      let currentPosition = this.position;
+    // Move method (functions is called methods when part of a class)
+    // This function calculates two vectors; one away from the target parameter
+    // and one towards the orginal position of the pixel particle. These vectors are 
+    // then used to translate the pixel particle position
+    move(target) { 
       
-      //age += deltaTime;
-      let fromTarget = p5.Vector.sub(currentPosition, target);
-      fromTarget.z = 0;
+      // calculate a vector from the target towards the pixel particle position
+      let fromTarget = p5.Vector.sub(this.position, target);
+      // limit the lenght of this vector to 1 (like normalizing without making the length 1)
       fromTarget.limit(1);
-      //fromTarget = fromTarget.mult(0.1);
-
-      let toOriginal = p5.Vector.sub(this.originalPosition, currentPosition);
-      //toOriginal.z = 0;
-      toOriginal.limit(20);
-      //print(toOriginal);
       
-      //this.position = this.position + fromTarget;// * deltaTime * 0.001;
-    
-      //if(fromTarget.magSq() < 0.45*0.45){      
-        this.position.x += fromTarget.x * deltaTime * 0.08;
-        this.position.y += fromTarget.y * deltaTime * 0.08;
-      //}
-      this.position.x += toOriginal.x * deltaTime * 0.01;
-      this.position.y += toOriginal.y * deltaTime * 0.01;
+      // calculate a vector from the particle position towards original pixel particle position
+      let toOriginal = p5.Vector.sub(this.originalPosition, this.position);
+      // limit the lenght of this vector to 20
+      toOriginal.limit(20);
+
+      // add the fromTargets vector and toOriginal vector to the current pixel particle 
+      // position, scaling it by delta time to move the same distance in all frame rates
+      // and then scaling the translation with the private members fromTargetScale and toOriginalScale
+      // that controls the significance of each movement
+      this.position.x += fromTarget.x * deltaTime * this.#fromTargetScale;
+      this.position.y += fromTarget.y * deltaTime * this.#fromTargetScale;
+      this.position.x += toOriginal.x * deltaTime * this.#toOriginalScale;
+      this.position.y += toOriginal.y * deltaTime * this.#toOriginalScale;
     }
 
+    // Display method (functions is called methods when part of a class)
+    // This function draws a square with a calulated size at the position of this pixel particle
     display() {
-     
-      fill(this.c); // Change the color
+      // Change the color of fills to the particle color
+      fill(this.particleColor); 
 
+      // push a new transform context to the stack
       push();
+        // translate this transform context to the pixel particle current position
         translate(this.position);
-        square(0,0, 1/(1/pixelStep) + 1.1 - (1/pixelStep));
+        
+        // draw a square in the center of this transform context effecivly this call draws 
+        // the squarre visibly at the pixel particle position although it's drawn at it's own origo
+        // this seems faster than drawing the square at the correct position without translating 
+        // the transform.
+        // The size of the square is calculated based of the pixelStep used when parsing the image.
+        // Pixel step of 1 results in a size of 1 + 1.1 - 1/1 => 1.1
+        // Pixel step of 2 results in a size of 2 + 1.1 - 1/2 => 2.6
+        // Pixel step of 3 results in a size of 3 + 1.1 - 1/3 => ~3.8
+        square(0,0, pixelStep + 1.1 - (1/pixelStep));
+      
+      // pop the transform context from the stack
       pop();
     }
   }
--- p5/exempel-pixelparticles/sketch.js
+++ p5/exempel-pixelparticles/sketch.js
@@ -1,58 +1,111 @@
-/*
-PixelParticles by Jonas Svegland 2021
-
-Copying and distribution of this file, with or without modification,
+/*Copying and distribution of this file, with or without modification,
 are permitted in any medium without royalty provided the copyright
 notice and this notice are preserved.  This file is offered as-is,
 without any warranty.*/
 
+/*
+PixelParticles sketch using p5js by Jonas Svegland 2021
+
+This sketch loads an image and converts it's pixels to particles 
+that avoid a point in space (currently mouse cursor) and tries to 
+get back to it's original position.*/
+
+// Array holding instanciated pixel particle objects
 var pixelParticles = [];
-var numPixels;
+
+// Object holding the loaded image
 var img;
-var fr = 30;
-var cam;
-var pixelStep = 4;
 
+// The frame rate we want to aim for
+var frameRate = 30;
 
+// Object holding the created 3D camera
+var camera;
+
+// Number of steps taken whe looping through pixels in the image 
+// to create particles. Increase to create less pixel particles 
+// 1 = create a particle for each pixel, 
+// 2 = create a particle every other pixel etc.
+const pixelStep = 2;
+
+// variable to count the number of pixels 
+var numPixels;
+
+// Preload function
+// Preloads an image so that it's alread done loading before running setup
 function preload() {
-  img = loadImage('Samuel_Atkins_Eliot_by_Gilbert_Stuart.jpg');
+  // Load an image from an URL
+  img = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Samuel_Atkins_Eliot%2C_by_Gilbert_Stuart.jpg/389px-Samuel_Atkins_Eliot%2C_by_Gilbert_Stuart.jpg');
 }
 
-
-function setup() { 
+// Setup function
+// Setup up everything before calling draw
+function setup() {
+  // create a canvas
   createCanvas(1024, 768 , WEBGL);
-  frameRate(fr);
-  //pixelDensity(1);
 
+  // set the frame rate we aim for
+  frameRate(frameRate);
+
+  // resize the loaded image by a third, custom for this exact image
+  img.resize(img.width/3, img.height/3);
+
+  // create and position the camera closer to the image
+  camera = createCamera();
+  camera.move(0,0, -400);
+  
+  // reset the numPixels counter
   numPixels = 0;
 
-  cam = createCamera();
-  cam.move(0,0, -200);
-  
+  // start creating the pixel particles
   go();
 }
 
+// Go function
+// This function instanciates objects to represent pixels from an image
 function go() {
+
+  // variable to hold the index of the next available slot in the pixel particle array
   let pixelIndex = 0;
+
+  // load pixles from the image into an array that we can parse through
   img.loadPixels();
 
+  // get the current pixel density (most surely 1 in most cases)
   const d = pixelDensity();
-  print("Img w,h " + img.width + ", " + img.height);
+  
+  // Debug width and height of the image, uncomment the line below if needed
+  // print("Img w,h " + img.width + ", " + img.height);
 
+  // Iterate through the image pixel array based on image width and height
+  // using the pixelStep variable as an increment
   for (let x = 0; x < img.width; x=x+pixelStep) {
     for (let y = 0; y < img.height; y=y+pixelStep) {
-      const i = 4 * d*(y * d*img.width + x);
-      const [r, g, b] = [img.pixels[i], img.pixels[i + 1], img.pixels[i + 2]]; // get colors
-      pixelParticles[pixelIndex] = new PixelParticle(x, y, color(r,g,b));
+      // calculate an index in the image pixel array taking in account the pixel density (if any)
+      let i = 4 * d*(y * d*img.width + x);
+      
+      // get the red, green and blue color of the pixel (ignoring any alpha)
+      let red = img.pixels[i];
+      let green = img.pixels[i+1];
+      let blue = img.pixels[i+2];
+
+      // instanciate a PixelParticle object with this pixels position and color
+      pixelParticles[pixelIndex] = new PixelParticle(x, y, color(red,green,blue));
+      
+      // randomize the pixleParticles position by calling the scramble function
       pixelParticles[pixelIndex].scramble(width, height);
       
+      // increment the index
       pixelIndex++;
+      // increment the pixel counter
       numPixels++;
     }
   }
 
-  print("Num pixels counted: " + numPixels);
-  print("Num PixelParticles generated: " + (pixelIndex));
+  // Debug the number of pixels counted, uncomment the line below if needed
+  // print("Num pixels counted: " + numPixels);
+  // Debug the number of pixels particles created, uncomment the line below if needed
+  //print("Num PixelParticles generated: " + (pixelIndex));
 }
 
 function draw() {
